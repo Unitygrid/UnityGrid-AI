f56c41974de6550ba44c330b9d8679ffcafc5719
Tahirasif31 <Tahirasif31@gmail.com>
2024-06-14 11:04:44 -0700

    Update test_integration.py

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,11 +1,11 @@
-import pytest
+import unittest
 from frontend.app import app, db
-from config import test_config
 
-@pytest.fixture
-def client():
-    app.config.from_object(test_config)
-    with app.test_client() as client:
-        with app.app_context():
-            db.create_all()
-        yield client
+class TestIntegration(unittest.TestCase):
+
+    def test_app(self):
+        self.assertIsNotNone(app)
+        self.assertIsNotNone(db)
+
+if __name__ == '__main__':
+    unittest.main()
