6ad4007c445703c7008c5dbc796ab6122a906c63
Tahirasif31 <Tahirasif31@gmail.com>
2024-06-14 11:54:43 -0700

    indentation error fixec

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -71,15 +71,15 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 # Initialize logging
-if not app.debug:
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/adios.log', maxBytes=10240, backupCount=10)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('A.D.I.O.S startup')
+    if not app.debug:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/adios.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('A.D.I.O.S startup')
 
 @app.errorhandler(500)
 def internal_error(error):
