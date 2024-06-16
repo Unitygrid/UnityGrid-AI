c071cf603a727e6f9e5720d0fcc77539025d61c5
Tahirasif31 <Tahirasif31@gmail.com>
2024-06-16 13:09:21 -0700

    Fix import paths and define predict and secure_data functions

---
+++ src/ai_modules/blockchain/blockchain.py
@@ -0,0 +1,20 @@
+from web3 import Web3
+import os
+
+# Use Alchemy's URL as the provider
+alchemy_url = os.getenv('ALCHEMY_URL', 'https://eth-mainnet.alchemyapi.io/v2/YOUR_API_KEY')
+web3 = Web3(Web3.HTTPProvider(alchemy_url))
+
+# Contract ABI and address
+contract_abi = [
+    # ABI details here...
+]
+contract_address = 'YOUR_CONTRACT_ADDRESS'
+
+# Initialize contract
+contract = web3.eth.contract(address=contract_address, abi=contract_abi)
+
+def secure_data(description):
+    tx_hash = contract.functions.logTask(description).transact({'from': web3.eth.accounts[0]})
+    tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+    return tx_receipt
